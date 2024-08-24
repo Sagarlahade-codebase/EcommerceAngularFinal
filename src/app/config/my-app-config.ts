1478725a09d91e9ebff9de5d08b69e4f5cf3aa35
Sagar Lahade <sagarlahade.developer@gmail.com>
2024-08-24 18:48:06 +0530

    initial commit

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: "0oaj4mywz4EPbqj0q5d7",
+    issuer: "https://dev-79071851.okta.com/oauth2/default",
+    redirectUri: "http://localhost:4200/login/callback",
+    scopes: ["openid", "profile", "email"],
+  },
+};
