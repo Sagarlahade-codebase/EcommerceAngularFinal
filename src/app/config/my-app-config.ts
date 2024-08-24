5f9adfce46d01c1b84e73cb0695d5d3afcd15713
Sagar Lahade <166719705+Sagarlahade-codebase@users.noreply.github.com>
2024-08-24 21:16:54 +0530

    Update my-app-config.ts

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: "0oaj4mywz4EPbqj0q5d7",
     issuer: "https://dev-79071851.okta.com/oauth2/default",
-    redirectUri: "http://localhost:4200/login/callback",
+    redirectUri: "https://sagarlahade-ecommerce-angular.vercel.app/login/callback",
     scopes: ["openid", "profile", "email"],
   },
 };
